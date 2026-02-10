8de27c2de663c96032e3614b7d710d465509d62b
혜인 <eomhyein04@gmail.com>
2026-02-10 18:18:19 +0900

    Add files via upload

---
+++ 2798.py
@@ -0,0 +1,16 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+card = list(map(int, sys.stdin.readline().split()))
+
+result = 0
+
+for i in range(N) :
+    for j in range(i + 1, N) :
+        for k in range(j + 1, N) :
+            x = card[i] + card[j] + card[k] 
+
+            if x <= M : 
+                result = max(result, x)
+
+print(result)
